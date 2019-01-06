11c531a44e7e93ef22a23532a4fa7550c1c98906
Kyle <polysplitter@gmail.com>
2019-01-06 14:37:16 -0600

    templates and more pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 let items = []
+let workItems = []
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
@@ -17,13 +18,32 @@ app.get('/', (req, res, next) => {
 
     let day = today.toLocaleDateString('en-US', options)
 
-    res.render('list', { kindOfDay: day , newItems: items })
+    res.render('list', { listTitle: day , newItems: items })
 })
 
 app.post('/', (req, res, next) => {
-    item = req.body.newItem
-    items.push(item)
-    res.redirect('/')
+    let item = req.body.newItem
+    if(req.body.list === 'Work') {
+        workItems.push(item)
+        res.redirect('/work')
+    } else {
+        items.push(item)
+        res.redirect('/')
+    }
+})
+
+app.get('/work', (req, res, next) => {
+    res.render('list', { listTitle: 'Work List', newItems: workItems})
+})
+
+app.post('/work', (req, res, next) => {
+    let item = req.body.newItem
+    workItems.push(item)
+    res.redirect('/work')
+})
+
+app.get('/about', (req, res, next) => {
+    res.render('about')
 })
 
 module.exports = app
