3582314d0ec605b66653430cabdb28e28e1ea71b
Kyle <polysplitter@gmail.com>
2019-01-06 13:18:07 -0600

    passing data

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res, next) => {
-
     let today = new Date()
-    let day = ""
-    if(today.getDay() === 6 || today.getDay() === 0) {
-        day = 'Weekend'
-        res.render('list', {foo: day})
-    } else {
-        day = 'Weekday'
-        res.render('list', {foo: day})
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
     }
+
+    let day = today.toLocaleDateString('en-US', options)
+
+    res.render('list', { kindOfDay: day })
+})
+
+app.post('/', (req, res, next) => {
+    let item = req.body.newItem
+    console.log(item)
+    res.redirect('/')
 })
 
 module.exports = app
