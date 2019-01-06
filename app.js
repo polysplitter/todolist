3de3116598b9595fa4c1e63440f722d338bca4bc
Kyle <polysplitter@gmail.com>
2019-01-05 23:02:32 -0600

    building a todo list application

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res, next) => {
-    res.send("Hello")
+
+    let today = new Date()
+    let day = ""
+    if(today.getDay() === 6 || today.getDay() === 0) {
+        day = 'Weekend'
+        res.render('list', {foo: day})
+    } else {
+        day = 'Weekday'
+        res.render('list', {foo: day})
+    }
 })
 
 module.exports = app
