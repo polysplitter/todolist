cfeb790f4d72fa9122b6a76b6745a37342f88a5c
Kyle <polysplitter@gmail.com>
2019-01-09 18:03:06 -0600

    initail mongodb setup for todolist.

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
 const express = require('express')
 const date = require(__dirname + '/date')
+const mongoose = require('mongoose')
 const app = express()
-
-// you can push items to an const array, but you cannot reassign the array to a different array.
-const items = ['Buy Food', 'Cook Food', 'Eat Food']
-const workItems = []
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser:true})
+
+const itemsSchema = {
+    name: String
+}
+
+const Item = mongoose.model('Item', itemsSchema)
+
+const item1 = new Item({
+    name: "Welcome to your todolist"
+})
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+})
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3]
+
 app.get('/', (req, res, next) => {
     let day = date.getDate()
-    res.render('list', { listTitle: day , newItems: items })
+
+    Item.find({}, (err, result) => {
+        if(!Array.isArray(result) || !result.length) {
+            Item.insertMany(defaultItems, (err) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    console.log('default items where loaded.')
+                }
+            })
+            res.redirect('/')
+        } else {
+            res.render('list', { listTitle: day , newItems: result })
+        }
+    })
 })
 
 app.post('/', (req, res, next) => {
